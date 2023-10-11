9d8160a65cdec12db4a7c2db2b2e02a0a4cdc58e
Carl Michael <carlliboon12@gmail.com>
2023-10-11 22:10:21 +0800

    Second commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,27 @@
+import { arrowRight } from "../assets/icons"
+import Button from "../components/Button"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative
+      xl:2-2/5 flex flex-col justify-center items-start
+      w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <Button label="Shop now" iconUrl={arrowRight} />
+      </div>
+
+    </section>
   )
 }
 
